4aad05ac6befe33e93888dc176de9548cf1946f5
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-09 22:40:32 +0500

    2.4.5

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,21 @@ import Header from "../components/Header";
 import BgDop from "../components/BgDop";
 import Fotter from "../components/Fotter";
 import Main from "../components/Main";
+import Button from "../components/Button";
 import BackBtn from "../components/BackBtn";
 import { useAuth } from "../stores/AuthContext";
 
 export default function NotFound() {
   const { user } = useAuth();
+
+  const logout = (e) => {
+    e.preventDefault();
+
+    
+
+    console.log("Выход..");
+  };
+
   return (
     <>
       <Header />
@@ -16,20 +26,21 @@ export default function NotFound() {
       <Main>
         <BackBtn />
         <h1 style={{ marginTop: "20%", color: "#963736" }}>{user.name}</h1>
-              <div>
-                  <div>
-                      <span>{user.phone}</span>
-                      <p>Телефон</p>
-                  </div>
-                  <div>
-                      <span>{user.name}</span>
-                      <p>Имя</p>
-                  </div>
-                  <div>
-                      <span>{user.rating}</span>
-                      <p>Рейтинг</p>
-                  </div>
+        <div>
+          <div>
+            <span>{user.phone}</span>
+            <p>Телефон</p>
+          </div>
+          <div>
+            <span>{user.name}</span>
+            <p>Имя</p>
+          </div>
+          <div>
+            <span>{user.rating}</span>
+            <p>Рейтинг</p>
+          </div>
         </div>
+        <Button style={{ padding:"10px 15px", fontSize:"15px" }} onClick={logout}>Выйти из аккаунта</Button>
       </Main>
 
       <Fotter />
