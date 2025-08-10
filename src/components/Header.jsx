dec7260ef5127ea5bbf2ef6c0d026c420dd2b980
RadislaveTK <160173614+RadislaveTK@users.noreply.github.com>
2025-08-10 18:33:39 +0000

    2.4.5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,21 @@ import "../styles/Header.css";
 
 export default function Header() {
   return (
-    <div style={{
-          width: "100%",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          zIndex: 20,
-    }}>
+    <div
+      style={{
+        width: "100%",
+        position: "fixed",
+        left: 0,
+        top: 0,
+        zIndex: 20,
+        height: "15%",
+        maxHeight: "80px",
+        minHeight: "80px",
+      }}
+    >
       <header>
-        
         <Link className="logo" to="/">
-          <img width={155} height={70} src="/assets/name.png" alt="Icon"/>
+          <img width={155} height={70} src="/assets/name.png" alt="Icon" />
         </Link>
       </header>
       <BurgerMenu />
