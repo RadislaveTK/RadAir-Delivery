4c0caba69f33f4a3821ca38f2d2ddbf061021c64
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-12 04:23:53 +0500

    2.4.54 (Fix карты)

--- src/components/GeoModal.jsx
+++ src/components/GeoModal.jsx
@@ -183,7 +183,7 @@ export default function GeoModal() {
         mapInstanceRef.current = null;
       }
     };
-  }, [isOpen, markerCoords]);
+  }, [isOpen]);
 
   // Геокодирование координат
   useEffect(() => {
