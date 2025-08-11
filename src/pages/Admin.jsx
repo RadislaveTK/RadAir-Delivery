c565b8f9d28b9183291189cd8fbab7b9e515998e
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-11 18:34:52 +0500

    2.4.8

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -37,7 +37,7 @@ export default function Admin() {
       const res = await fetch(
         "https://radair-delivery-back-production-21b4.up.railway.app/api/product/create_product",
         {
-          method: "GET",
+          method: "POST",
           body: fd, // не ставим Content-Type, fetch сам проставит multipart/form-data
         }
       );
