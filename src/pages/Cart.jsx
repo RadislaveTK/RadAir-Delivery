9ec01a66f9f048dc8811f780589eb957c42a9b3e
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-13 01:54:28 +0500

    2.4.72

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-
 import Header from "../components/Header";
 import BgDop from "../components/BgDop";
 import Fotter from "../components/Fotter";
@@ -7,7 +6,6 @@ import Button from "../components/Button";
 import BackBtn from "../components/BackBtn";
 
 export default function Cart() {
-  
   return (
     <>
       <Header />
@@ -15,35 +13,13 @@ export default function Cart() {
 
       <Main>
         <BackBtn />
-        {/* <h1 style={{ marginTop: "20%", color: "#963736" }}>{user.name}</h1> */}
-        <div className="profile-block">
-          <div className="profile-inp">
-            <div>
-              <p>Телефон</p>
-              <span>{user.phone}</span>
-            </div>
-            <img src="/assets/icons/phone.svg" alt="phone" />
-          </div>
-          <div className="profile-inp">
-            <div>
-              <p>Имя</p>
-              <span>{user.name}</span>
-            </div>
-            <img src="/assets/icons/badge.svg" alt="badge" />
-          </div>
-          <div className="profile-inp">
-            <div>
-              <p>Рейтинг</p>
-              <span>{user.rating}</span>
-            </div>
-            <img src="/assets/icons/star.svg" alt="star" />
-          </div>
-          <Button
-            style={{ padding: "10px 15px", fontSize: "15px" }}
-            onClick={logout}
-          >
-            Выйти из аккаунта
-          </Button>
+
+        <h1 style={{ marginTop: "20%", color: "#963736" }}>Корзина</h1>
+
+        <hr />
+
+        <div className="block">
+            
         </div>
       </Main>
 
