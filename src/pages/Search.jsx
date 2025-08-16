4b5dd0411c5503af37430b47f5ca138110d72beb
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-16 22:39:54 +0500

    2.4.73

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -70,7 +70,7 @@ export default function SearchP() {
     if (!nextPageUrl) return;
 
     setLoading(true);
-    fetch(nextPageUrl)
+    fetch("https://radair-delivery-back-production-21b4.up.railway.app/api/product/search?page="+(page+1))
       .then((res) => res.json())
       .then((data) => {
         setProducts((prev) => [...prev, ...data.data]);
