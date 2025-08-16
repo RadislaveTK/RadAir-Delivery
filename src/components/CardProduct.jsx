7c5360d9b6467af3163d3fb2f6d558ff9efadc1f
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-17 03:21:50 +0500

    2.4.87

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -20,14 +20,14 @@ export default function CardProduct({ product, children, ...props }) {
             onClick={(e) => e.stopPropagation()} // чтобы клик внутри окна не закрывал модалку
           >
             <button className="modal-close" onClick={closeModal}>
-              <img src="/assets/icons/close.svg"  alt="close" />
+              <img src="/assets/icons/close.svg" width={30} height={30} alt="close" />
             </button>
             <img src={product.img} alt={product.name} className="modal-img" />
-            <h2>{product.name}</h2>
-            <p><b>Описание:</b> {product.desc}</p>
-            <p><b>Производитель:</b> {product.producer}</p>
-            <p><b>Страна:</b> {product.country}</p>
-            <p><b>Цена:</b> {product.price} тг</p>
+            <h2 style={{ textAlign: "left" }}>{product.name}</h2>
+            <p style={{ textAlign: "left" }}><b>Описание:</b> {product.desc}</p>
+            <p style={{ textAlign: "left" }}><b>Производитель:</b> {product.producer}</p>
+            <p style={{ textAlign: "left" }}><b>Страна:</b> {product.country}</p>
+            <p style={{ textAlign: "left" }}><b>Цена:</b> {product.price} тг</p>
           </div>
         </div>
       )}
